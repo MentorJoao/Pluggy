b9389d1488414f89cdc5d9e7b85a1c0596841425
MentorJoao <150355171+MentorJoao@users.noreply.github.com>
2023-11-15 17:45:10 +0000

    Update token.ts

--- token.ts
+++ token.ts
@@ -5,8 +5,8 @@ const { PLUGGY_CLIENT_ID, PLUGGY_CLIENT_SECRET } = process.env
 
 export default function (req: VercelRequest, res: VercelResponse) {
   const pluggyClient = new PluggyClient({
-    clientId: PLUGGY_CLIENT_ID,
-    clientSecret: PLUGGY_CLIENT_SECRET
+    clientId: "8f6e1465-ed6e-44c6-915a-87db281eb694",
+    clientSecret: "732d4631-183c-44d9-bb17-9503f393ec94"
   })
 
   const { itemId, options } = req.body || {}
